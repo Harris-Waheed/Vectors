f7787dd8dc5b78dd0671c7a614855553afa2ec3f
Haris Waheed <ah6086395@gmail.com>
2026-03-10 06:26:45 +0500

    Practice File Removed!

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import inventory, customers
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware,
+                   allow_origin='http://localhost:3000',
+                   allow_methods='*',
+                   allow_header='*',
+                   allow_credentials=True
+                   )
 
 app.include_router(inventory.router)
 app.include_router(customers.router)
